865203bea4ac5995c718913d664d3196115d9a30
Humberto Ribeiro <humbertofox@hotmail.com>
2024-05-13 18:50:36 -0300

    upgrade header e css App

---
+++ src/components/header/header.jsx
@@ -0,0 +1,90 @@
+import React, {
+    useState
+} from "react"
+import {
+    HeaderMenu,
+    ImgEnterprise
+} from "../../style/headerstyle";
+import EnterpriseImg from "../../assets/dinheiro.png";
+import ClothesImg from "../../assets/doacao-de-roupas.png";
+import UserImg from "../../assets/novo-usuario.png";
+import DonationImg from "../../assets/doacao.png";
+import VerifiedUserImg from "../../assets/usuario-verificado.png";
+import AgendaImg from "../../assets/agenda.png";
+import ConfirmImg from "../../assets/confirmar.png";
+import ReportImg from "../../assets/relatorio.png";
+
+export const Header = () => {
+
+    const [navMenuLi, SetNavMenuLi] = useState("Relatório");
+
+    const handlesMenuLiClick = (element) => {
+        SetNavMenuLi(element);
+    };
+
+    return (
+        <HeaderMenu>
+            <ImgEnterprise src={EnterpriseImg} alt="Logo Empresa" />
+            <nav>
+                <ul>
+                    <li title="Cadastrar Doação"
+                        name="cadastro_doacao"
+                        className={navMenuLi === "Cadastrar Doação" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Cadastrar Doação")}
+                    >
+                        <img src={ClothesImg} alt="Icon Doação" />
+                        <span>Cadastrar Doação</span>
+                    </li>
+                    <li title="Cadastrar Doador"
+                        name="cadastro_doador"
+                        className={navMenuLi === "Cadastrar Doador" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Cadastrar Doador")}
+                    >
+                        <img src={UserImg} alt="Icon Doador" />
+                        <span>Cadastrar Doador</span>
+                    </li>
+                    <li title="Editar Doação"
+                        name="editar_doacao"
+                        className={navMenuLi === "Editar Doação" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Editar Doação")}
+                    >
+                        <img src={DonationImg} alt="Icon Editar Doação" />
+                        <span>Editar Doação</span>
+                    </li>
+                    <li title="Editar Doador"
+                        name="editar_doador"
+                        className={navMenuLi === "Editar Doador" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Editar Doador")}
+                    >
+                        <img src={VerifiedUserImg} alt="Icon Editar Doador" />
+                        <span>Editar Doador</span>
+                    </li>
+                    <li title="Agendar Coleta"
+                        name="agendar_coleta"
+                        className={navMenuLi === "Agendar Coleta" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("CAgendar Coleta")}
+                    >
+                        <img src={AgendaImg} alt="Icon Agendar coleta" />
+                        <span>Agendar Coleta</span>
+                    </li>
+                    <li title="Confirmar Coleta"
+                        name="confirmar_coleta"
+                        className={navMenuLi === "Confirmar Coleta" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Confirmar Coleta")}
+                    >
+                        <img src={ConfirmImg} alt="Icon Confirmar Coleta" />
+                        <span>Confirmar Coleta</span>
+                    </li>
+                    <li title="Relatório"
+                        name="gerar_relatorio"
+                        className={navMenuLi === "Relatório" ? "active" : ""}
+                        onClick={() => handlesMenuLiClick("Relatório")}
+                    >
+                        <img src={ReportImg} alt="Icon Relatório" />
+                        <span>Gerar Relatório</span>
+                    </li>
+                </ul>
+            </nav>
+        </HeaderMenu>
+    )
+}
---
+++ src/style/headerstyle.jsx
@@ -0,0 +1,69 @@
+import styled, { css } from "styled-components";
+
+const HeaderMenu = styled.header`
+    width: 250px;
+    height: 100vh;
+    display: flex;
+    position: fixed;
+    flex-direction: column;
+    align-self: flex-start;
+    border-right: solid 3px #79D1FF;
+    background-color: #AAF998;
+    transition: .3s ease-in-out;
+
+    nav,
+    ul {
+        width: 100%;
+        background-color: #AAF998;
+    }
+    ul {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+    }
+    li {
+        display: flex;
+        align-items: center;
+        padding: 10px;
+        background-color: #AAF998;
+        transition: .3s ease-in-out;
+        cursor: pointer;
+    }
+    li:hover {
+        background-color: #79D1FF;
+    }
+    nav img {
+        width: 40px;
+        margin-right: 20px;
+        background: none;
+    }
+    .active {
+        background-color: #79D1FF;;
+    }
+    span {
+        font-size: 18px;
+        background: none;
+        transition: .3s ease-in-out;
+    }
+    @media (max-width: 1080px) {
+        width: 75px;
+        
+        span {
+            display: none;
+        }
+    }
+`;
+
+const ImgEnterprise = styled.img`
+    display: flex;
+    width: 100px;
+    background: none;
+    padding: 20px 0;
+    margin: 0 auto;
+    transition: .3s ease-in-out;
+    @media (max-width: 1080px) {
+        width: 50px;
+    }
+`;
+
+export { HeaderMenu, ImgEnterprise };
